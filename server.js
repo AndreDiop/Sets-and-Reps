d8039b4c14327a72eec58737014e4645f1584dd3
Andre Diop <itsjustandre@gmail.com>
2021-02-07 15:06:32 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
-const Workout = require("./models/workout");
 const app = express();
+const htmlRoutes = require("./routes/htmlRoutes");
+// require("dotenv").config();
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +18,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useCreateIndex: true,
 });
 
+// Api routes
+app.use(require("./routes/api.js"));// 
+
+// View  routes
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
